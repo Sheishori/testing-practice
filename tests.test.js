844df976d75ed1613aa54ce7cf418002ec067ab2
Sheishori <sheishori@gmail.com>
2023-01-11 12:13:27 +0100

    Add tests for a calculator function

--- tests.test.js
+++ tests.test.js
@@ -1,5 +1,6 @@
 import capitalize from './capitalize';
 import reverseString from './reverseString';
+import calculator from './calculator';
 
 test('Capitalizing', () => {
 	expect(capitalize('Cat')).toBe('CAT');
@@ -11,4 +12,20 @@ test('Passing numbers', () => {
 
 test('Reverse string', () => {
 	expect(reverseString('Cat')).toBe('taC');
+});
+
+test('Add', () => {
+	expect(calculator.add(3, 3)).toBe(6);
+});
+
+test('Subtract', () => {
+	expect(calculator.subtract(3, 3)).toBe(0);
+});
+
+test('Divide', () => {
+	expect(calculator.divide(3, 3)).toBe(1);
+});
+
+test('Multiply', () => {
+	expect(calculator.multiply(3, 3)).toBe(9);
 });
